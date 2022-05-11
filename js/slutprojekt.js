48b4c16569ff1cd768f1bb6f983e779f19396d82
FrejaPayne <94685221+FrejaPayne@users.noreply.github.com>
2022-05-11 13:34:14 +0200

    Add files via upload

--- js/slutprojekt.js
+++ js/slutprojekt.js
@@ -1,27 +1,24 @@
-"use strict";
-let slideIndex = 1;
-showSlides(slideIndex);
+'use strict';
+var slideIndex = 0;
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000);
+}
+showSlides();
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += "responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
